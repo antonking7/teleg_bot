c4c7343464fd83d350516636c01e8fd6ca403421
antonking7 <anton.nash93@gmail.com>
2022-03-21 15:12:27 +0300

    vers2

--- bot_run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call %~dp0venv\Scripts\activate
-
-set TOKEN=5106943049:AAEJEjdb1ctamaGob8r3u6vbeAhGN7ZRkSo
-
-python main.py
-
-pause
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import config
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 import os
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler()
 async def echo_send(message : types.message):
 	await message.answer(message.text)
 
-executor.start_polling(dp, skip_update=True)
+executor.start_polling(dp)
