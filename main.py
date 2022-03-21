25b8de65fd2cc76d740cfbf46bed731cea552d56
antonking7 <anton.nash93@gmail.com>
2022-03-21 15:45:59 +0300

    db

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import config
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
-import os
+from datetime import datetime
 
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
 
+from db import BotDB
+BotDB = BotDB('assistent.db')
+
 @dp.message_handler()
 async def echo_send(message : types.message):
-	await message.answer(message.text)
+	# await message.answer(message.text)
+	await message.answer(datetime.today())
 
 executor.start_polling(dp)
